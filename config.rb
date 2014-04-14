b9a7ad1f14f9ea3ae8ff07c7d7008ff059a42735
Dmitry Nikitenko <dima.nikitenko@gmail.com>
2014-04-14 14:48:51 +0700

    Save sprites to the build directory

--- config.rb
+++ config.rb
@@ -5,6 +5,7 @@ http_path = "../"
 css_dir = "dist/css"
 sass_dir = "sass"
 images_dir = "img"
+generated_images_dir = "dist/img"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
